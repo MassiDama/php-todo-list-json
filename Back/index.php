fd9ab6ce665f17df1fd7727a497742d6a2927978
Massidama <massidama@outlook.it>
2024-01-25 18:09:05 +0100

    feat: i vite

---
+++ Back/index.php
@@ -0,0 +1,23 @@
+<?php
+    header('Content-Type:application/json');
+
+    $todos = [
+        [
+            'task' => 'cucinare',
+            'done' => true,
+        ],
+        [
+            'task' => 'andare in chiesa',
+            'done' => true,
+        ],
+        [
+            'task' => 'fare i compiti',
+            'done' => false,
+        ],
+        [
+            'task' => 'comprare un gatto',
+            'done' => false,
+        ],
+    ];
+
+?>
